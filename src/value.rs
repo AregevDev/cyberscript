1399c7b71c6e8ef98ff92cc35793e72b0f669449
Alon Regev <aregevdev@gmail.com>
2024-04-14 20:31:53 +0300

    Add conversions from Value to other primitive types

--- src/value.rs
+++ src/value.rs
@@ -1,12 +1,24 @@
-use cyberscript_sys::{clAsInteger, CLValue};
+use cyberscript_sys::{clAsBool, clAsFloat, clAsInteger, CLValue};
 
 #[derive(Debug, Default, Copy, Clone)]
 pub struct Value {
     pub inner: CLValue,
 }
 
+impl From<Value> for bool {
+    fn from(value: Value) -> Self {
+        unsafe { clAsBool(value.inner) }
+    }
+}
+
 impl From<Value> for i64 {
     fn from(value: Value) -> Self {
         unsafe { clAsInteger(value.inner) }
+    }
+}
+
+impl From<Value> for f64 {
+    fn from(value: Value) -> Self {
+        unsafe { clAsFloat(value.inner) }
     }
 }
